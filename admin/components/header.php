1778458f48018331ec8f7568a64bf8888004ea63
JARP <andersonandy046@gmail.com>
2024-12-20 23:32:06 +0800

    title

--- admin/components/header.php
+++ admin/components/header.php
@@ -8,7 +8,7 @@ $db = new global_class();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Responsive Sidebar</title>
+  <title>FACSCHED</title>
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
